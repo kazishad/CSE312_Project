eddd3528ffe9724bb2decff63de5a65381dbb6ec
kazishad <kazishad@buffalo.edu>
2022-03-10 20:18:45 -0500

    removed unnecessary files

--- playground/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
-def say_hello(request):
-    return render(request,'sample.html',{'name':'World!'})
