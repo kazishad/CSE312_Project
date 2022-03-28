bca969987f2225bba76eff4c896ad5915c307f84
kazishad <kazishad@buffalo.edu>
2022-03-28 16:56:42 -0400

    updated README, implemented functions on auth_test for easy testing, and changed return type for create() function

--- auth_test.py
+++ auth_test.py
@@ -1,14 +1,43 @@
 import authentication
 
-# creating accounts
-authentication.create("David","123")
-authentication.create("Alex", "password")
-authentication.create("Maria", "qwerty")
+# example of calling the create function
+# authentication.create("David","123")
+# authentication.create("Alex", "password")
+# authentication.create("Maria", "qwerty")
 
-# verifying users
-authentication.verify("David", "123")
-authentication.verify("Alex", "password")
-authentication.verify("Maria", "qwerty")
+# example of calling the verify function 
+# authentication.verify("David", "123")
+# authentication.verify("Alex", "password")
+# authentication.verify("Maria", "qwerty")
 
-# non-existent account
-authentication.verify("Anna", "qwerty123")
+
+# Input is a dictionary in {"username":<username>,"password":<password>} format
+def create_multi_account(data:list):
+    for info in data:
+        if authentication.create(info["username"], info["password"]):
+            print("account created for", info["username"])
+        else:
+            print("error in creating account for", info["username"])
+    
+
+# Input is a dictionary in {"username":<username>,"password":<password>} format
+def verify_multi_account(data: list):
+    for info in data:
+        if authentication.verify(info["username"], info["password"]):
+            print("user", info["username"], "has been verified")
+        else:
+            print("user", info["username"], "could not be verified")
+
+
+# create_and_verify only exists because we are lazy lol
+def create_and_verify(data: list):
+    create_multi_account(data)
+    verify_multi_account(data)
+
+# creating multiple accounts
+account_info = [{"username":"David", "password":"123"}, 
+                {"username":"Alex", "password":"password"}, 
+                {"username":"Maria", "password":"qwerty"}]
+
+create_and_verify(account_info)
+        
--- authentication.py
+++ authentication.py
@@ -5,11 +5,13 @@ mc = MongoClient("mongo")
 db = mc["cse312"]
 cred_collection = db["credentials"]
 
-def create(username: str, password: str) -> str:
-    # implement salt here
+def create(username: str, password: str) -> bool:
+    
+    # creating an account
     hashed_pass = hashlib.sha512(password.encode()).hexdigest()
     cred_collection.insert_one({"username": username, "password":hashed_pass})
-    return "account created for " + username
+    return True
+
 
 def verify(username: str, password: str) -> bool:
     # search db to find hash
@@ -18,11 +20,11 @@ def verify(username: str, password: str) -> bool:
         hashed_pass = db_return["password"]
         current_hash = hashlib.sha512(password.encode()).hexdigest()
         if (hashed_pass == current_hash):
-            print("identity verified for", username)
+            # print("identity verified for", username)
             return True
         else:
-            print("wrong password or email for ", username)
+            # print("wrong password or email for ", username)
             return False
     else:
-        print("username does not exist")
+        # print("username does not exist")
         return False
