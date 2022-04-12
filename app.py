6a57a5850e911353dec2efac7923ffe559affca5
kazishad <kazishad@buffalo.edu>
2022-04-11 22:36:28 -0400

    added picture upload functionality

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
+import re
+from tabnanny import check
 from flask import Flask, render_template, request, redirect, url_for
+import os
 
 
 import db
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = './images'
 
 @app.route("/")
 def root():
@@ -31,6 +35,31 @@ def register():
     else:
         return render_template("Register.html")
 
+def check_allowed(input: str) -> bool:
+
+    extensions = {'jpg', 'png', 'jpeg'}
+    file_type = input.split('.')[1].lower()
+
+    if file_type in extensions:
+        return True
+    else:
+        return False
+
+@app.route("/upload", methods=["POST","GET"])
+def upload():
+    if (request.method == "GET"):
+        return render_template("upload_image.html")
+    elif (request.method == "POST"):
+
+        file = request.files['file']
+        filename = file.filename
+
+        if check_allowed(filename):
+
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return "Your File Has Been Saved!"
+        else:
+            return "Unsupported File Extension"
 
 if __name__ == '__main__':
   
