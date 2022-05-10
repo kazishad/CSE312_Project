39e9469415c0962338433956ce355506cc4dd947
Wren <42078212+Wrenmart@users.noreply.github.com>
2022-05-09 21:51:34 -0400

    Wren's handing off

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template, request, redirect, url_for
-
-
-
-
+from flask_socketio import SocketIO
+from custom_websockets import *
+from flask_socketio import SocketIO, emit
 
 import db
 
 app = Flask(__name__)
+socketio = SocketIO(app)
 
 @app.route("/")
 def root():
@@ -41,9 +41,30 @@ def register():
     else:
         return render_template("Register.html")
 
+# ---------------------------------------------------------------------------------
+@socketio.on('connect')
+def chat_connect():
+    print ('connected')
+
+
+@socketio.on('disconnect')
+def chat_disconnect():
+    print ("Client disconnected")
+
+
+@socketio.on('broadcast')
+def chat_broadcast(message):
+    print ("test")
+    emit("chat", {'data': message['data']})
+
+
+@socketio.on('message')
+def handle_message(data):
+    handle_websockets(data)
+
 
 if __name__ == '__main__':
   
     # run() method of Flask class runs the application 
     # on the local development server.
-    app.run()
+    socketio.run(app)
