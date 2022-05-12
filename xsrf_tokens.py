db4f01e6f798b6a6ee2f78bee0e66d1ff13921f5
Wren <42078212+Wrenmart@users.noreply.github.com>
2022-05-11 23:33:36 -0400

    removed print statements

--- xsrf_tokens.py
+++ xsrf_tokens.py
@@ -17,19 +17,15 @@ def generate_xsrf_token() -> str:
     # Generate XSRF token and embed into the html
     xsrf_token = secrets.token_urlsafe(10) # length of 10, as string
     xsrf_token_storage.insert_one({xsrf_token: 0}) # Store token in db
-    print_xsrf_tokens()
+    # print_xsrf_tokens()
     return xsrf_token
         
 
 def get_xsrf_tokens() -> list:
     stored_xsrf_tokens = xsrf_token_storage.find({}, {"_id": 0}) # Obtain all the tokens (remember they are in dictionary form)
-    # print(f"first tokens: {stored_xsrf_tokens}", flush=True)
     list_of_tokens = list(stored_xsrf_tokens)
-    # print(f"==> stored_xsrf_tokens was: {list_of_tokens}", flush=True)
     stored_xsrf_tokens_list = list_of_tokens # Create iterable for for-loop
-    # print(f">stored_xsrf_tokens_list: {stored_xsrf_tokens_list}", flush=True)
     stored_xsrf_tokens_dict = ChainMap(*stored_xsrf_tokens_list) # Flatten list of dicts to --> list
-    print(stored_xsrf_tokens_dict, flush=True)
     keys = list(stored_xsrf_tokens_dict.keys()) # The tokens are stored in the keys of the dict
     return keys
 
@@ -38,7 +34,6 @@ def validate_xsrf_token(xsrf_token: str) -> bool:
     """ Returns boolean: True if valid token | False if not found """
 
     xsrf_tokens = get_xsrf_tokens()
-    print(f"\n==> xsrf_tokens was {xsrf_tokens}", flush=True)
     for stored_xsrf_token in xsrf_tokens: # The tokens themselves are stored in the keys
         if xsrf_token == stored_xsrf_token:
             return True
