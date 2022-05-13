b0f05d7afbdc83f9494b2757ac5c63d1d2806798
aleena-sheikh <aleenabsheikh@gmail.com>
2022-05-13 00:23:12 -0400

    fixed small front end bugs

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ app.config['UPLOAD_FOLDER'] = './images'
 @app.route("/", methods=["POST", "GET"])
 def root():
     if "auth" not in request.cookies:
-        return '<div><h1>not logged in</h1><a href="/login">login</a><a href="/register">register</a></div>'
+        return '<div><h1>not logged in</h1><a href="/login">login</a></br><a href="/register">register</a></div>'
 
 
     template =  open("templates/index.html").read() 
@@ -36,14 +36,14 @@ def root():
     start_index = template.find(loop_start_tag)
     end_index = template.find(loop_end_tag)
 
-    final_content = (
+    content = (
             template[:start_index]
             + loop_content
             + template[end_index + len(loop_end_tag) :]
         )
-    return final_content
+    final_content = content.replace("{{user}}", user)
 
-    
+    return final_content
 
 
     
@@ -55,11 +55,7 @@ def profile(profile):
     if check_user(profile):
         if "auth" in request.cookies:
             authToken = request.cookies.get('auth')
-
             user = username_from_auth_token(authToken)
-
-            if user != None:
-                user = sanitize_data(user)
             if user:
                 if user == profile:
                     returnhtml = custom_render_template("templates/profile.html", "user", profile)
@@ -79,9 +75,9 @@ def profile(profile):
                 return returnhtml
                 
             else:
-                return '<div><h1>no user found</h1><a href="/login">login</a><a href="/register">register</a></div>'
+                return '<div><h1>no user found</h1><a href="/login">login</a></br><a href="/register">register</a></div>'
         else:
-            return '<div><h1>no user found</h1><a href="/login">login</a><a href="/register">register</a></div>'
+            return '<div><h1>no user found</h1><a href="/login">login</a></br><a href="/register">register</a></div>'
     else:
         return '<div><h1>Not a valid profile</h1><a href="/">Click here to go to the homepage</a></div>'
 
@@ -110,10 +106,10 @@ def login():
                 return response
                 
             else:
-                return '<div><h1>Invalid credentials</h1><a href="/login">login</a><a href="/register">register</a></div>'
+                return '<div><h1>Invalid credentials</h1><a href="/login">login</a></br><a href="/register">register</a></div>'
                 
         else:
-            return '<div><h1>Invalid xsrf token</h1><a href="/login">login</a><a href="/register">register</a></div>'
+            return '<div><h1>Invalid xsrf token</h1><a href="/login">login</a></br><a href="/register">register</a></div>'
     else:
         # Populate xsrf token in form
         xsrf_token = generate_xsrf_token()
